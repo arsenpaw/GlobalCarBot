53094e718f48a033a0866e4a113e699693188789
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-02-25 10:52:43 +0200

    Add logging to console and file Log.log

--- PythonBot/bot.py
+++ PythonBot/bot.py
@@ -1,9 +1,15 @@
+import logging
 import asyncio
 
 
 async def main():
-    print('Hello world')
-
+    file_log = logging.FileHandler('Log.log')
+    console_out = logging.StreamHandler()
+    logging.basicConfig(handlers=(file_log, console_out),
+                        format='[%(asctime)s | %(levelname)s]: %(message)s',
+                        datefmt='%m.%d.%Y %H:%M:%S',
+                        level=logging.INFO)
+    logging.info('Program Started !!!')
 
 if __name__ == "__main__":
     asyncio.run(main())
