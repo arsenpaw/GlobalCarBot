aa49d35fab1d39d1923fa060ddca0e149c36c0db
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-02-25 19:40:11 +0200

    bug fixes

--- PythonBot/Handlers/base_handlers.py
+++ PythonBot/Handlers/base_handlers.py
@@ -10,12 +10,12 @@ router = Router()
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    logging.log('/start command')
+    logging.info("/command start")
     await message.answer(f"<b>Привіт {message.from_user.full_name} !</b>\n Ми ітд ітп/help",
-                         reply_markup=start_keyboard.start_kb())
+                         reply_markup=start_keyboard.start_kb)
 
 
 @router.message(F.text.lower() == 'в головне меню')
 async def back_to_menu(message: Message):
-    logging.log('/main menu command')
-    await message.answer('Ви в головному меню', reply_markup=start_keyboard.start_kb())
+    logging.info("/main menu command")
+    await message.answer('Ви в головному меню', reply_markup=start_keyboard.start_kb)
