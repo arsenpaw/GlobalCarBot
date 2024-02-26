5ef3d5b6fcfcd84745b85af8e4941398921d76f0
Eextreeme <romkapyl@gmail.com>
2024-02-26 10:11:05 +0200

    adding-year-buttons

--- PythonBot/Handlers/cars_in_stock_handlers.py
+++ PythonBot/Handlers/cars_in_stock_handlers.py
@@ -9,12 +9,18 @@ from aiogram.fsm.context import FSMContext
 router = Router()
 
 @router.message(F.text.lower() == 'авто в наявності')
-<<<<<<< HEAD
 async def cars_cost_in_stock(message: Message):
-=======
-async def back_to_menu(message: Message):
->>>>>>> 411c817dc7c4d468c0bd2a4aff543edc9b97fb0a
     logging.info("cars in stock button")
     await message.answer('Який ваш комфортний бюджет для купівлі авто?', reply_markup=cars_in_stock_keyboard.cars_cost_in_stock_kb)
+    print(F.text.lower())
     
 
+@router.message(F.text.lower() == 'До 8 000$' 
+                or F.text.lower() == '8 000$ - 15 000$' 
+                or F.text.lower() == '15 000$ - 20 000$'
+                or F.text.lower() == '20 000$ +'
+                )
+async def cars_year_in_stock(message: Message):
+    logging.info("cars in stock button")
+    await message.answer('Автомобіль яких років випуску ви розглядаєте?', reply_markup=cars_in_stock_keyboard.cars_year_in_stock_kb)
+    car_cost = F.text.lower()
