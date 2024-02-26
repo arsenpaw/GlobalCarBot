2fff791981c9c1999165a2999a790f2a43d6baa1
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-02-26 09:09:28 +0200

    change status.py and solve bug when state machin doesnt updating

--- PythonBot/Handlers/estimated_cost_handlers.py
+++ PythonBot/Handlers/estimated_cost_handlers.py
@@ -14,7 +14,7 @@ router = Router()
 @router.message(F.text.lower() == 'вартість під ключ')
 async def estimated_cost_handler(message: Message,state:FSMContext) -> None:
     logging.info("/estimated cose")
-    await state.set_state(BotStates.get_estimnated_cost)
+    await state.set_state(BotStates.user_car_info)
     await message.answer(f"Ми зробимо для вас індивідуальний прорахуноквартості під ключ в Україні \n "
                          f"<b>Вкажіть, який автомобіль вас цікавить</b>\n \n "
                          f"- марка, модель та рік\n \n"
@@ -22,15 +22,20 @@ async def estimated_cost_handler(message: Message,state:FSMContext) -> None:
                          f"- тощо",
                          reply_markup = start_keyboard.consult_and_main_kb)
 
-@router.message(BotStates.get_estimnated_cost)
+@router.message(BotStates.user_car_info)
 async def wait_data_input(message: Message,state:FSMContext) -> None:
     logging.info("User input")
+    await state.update_data(user_car_info = message.text)
+    current_state = await state.get_state()
+
+    logging.info("Cancelling state %r", current_state)
     logging.info(message.text)
     if message.text.lower() == 'звязатись з менеджером':
+        await state.clear() #changes
         await connect_to_manager(message,state)
     else:
+        await state.clear()  # changes
         await message.answer(f"Інформацію збережено.")
         await message.answer(f"Щоб отрмати детальнішу інформацію по даному автомобілю "
                           f"\n натисніть 'Отримати прорахунок' aбо звяжіться з менеджером",
                          reply_markup=estimated_cost_keyboards.individual_cost_kb)
-
