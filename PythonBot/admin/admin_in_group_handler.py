7493f6594f1c81a64ddd756588cd5199934439d3
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-03-01 17:41:00 +0200

    continue dev admin,add message to unhandeled reqests

--- PythonBot/admin/admin_in_group_handler.py
+++ PythonBot/admin/admin_in_group_handler.py
@@ -1,8 +1,8 @@
-
+import logging
 
 from aiogram import F, Bot, types, Router
 from aiogram.filters import Command
-
+import sqlite3
 
 from filters.admin_filters import *
 admin_group_router = Router()
@@ -22,7 +22,30 @@ async def get_admins(message: types.Message, bot: Bot):
     bot.my_admins_list = admins_list
     if message.from_user.id in admins_list:
         await message.delete()
-    print(admins_list)
+    logging.info( bot.my_admins_list)
+
+@admin_group_router.message(Command("update"))
+async def get_aplies(message: types.Message, bot: Bot):
+    logging.info('send_aplies_to_admin')
+
+    with sqlite3.connect("database/clients.db") as db:
+        cur = db.cursor()
+        query = (""" SELECT * FROM CertainCar WHERE status = 'unhandled';
+               """)
+        cur.execute(query)
+        rows = cur.fetchall()
+        logging.info(f"SQL RESPONCE {rows}")
+        for person in rows:
+            name = person[2]
+            request = person[3]
+
+            phone_number = person[4]
+            phone_number =f'+ {phone_number}' if '+' not in phone_number else phone_number
+            await message.answer(f'Імя: {name}\n'
+                           f'Номер телефону  {phone_number}\n'
+                           f'Запит: {request}\n'
+                            f'Статус: Не оброблено')
+
 
 
 
