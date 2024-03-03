cc3d59fc8c8b4d4e0b2f912969a5b31d5e712972
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-03-03 13:44:17 +0200

    pep8 fixes and update readme

--- PythonBot/bot.py
+++ PythonBot/bot.py
@@ -4,15 +4,19 @@ import asyncio
 from admin.admin_methods import *
 from config_reader import config
 from aiogram import Bot, Dispatcher
-from Handlers import  *
+from Handlers import *
 from Handlers import carfax_get_handlers, unhandled_message
 from Handlers import car_to_find_handlers
 from admin.admin_in_group_handler import *
-async def run_function_every_two_minutes(bot:Bot):
+from admin.constants import SEC_TIME_TO_SLEEP
+
+
+async def run_function_every_two_minutes(bot: Bot):
     while True:
         logging.info('AUTOREQUEST')
         await auto_request_to_db(bot=bot)
-        await asyncio.sleep(60)
+        await asyncio.sleep(SEC_TIME_TO_SLEEP)
+
 
 async def main():
     file_log = logging.FileHandler('Log.log')
@@ -27,12 +31,13 @@ async def main():
     bot.chat_to_send = int()
     logging.info(f'Server is: {bot.session.api.base}')
     dp = Dispatcher(bot=bot)
-    dp.include_routers(base_handlers.router, estimated_cost_handlers.router, cars_in_stock_handlers.router, callback_user_chose_car.router,
+    dp.include_routers(base_handlers.router, estimated_cost_handlers.router, cars_in_stock_handlers.router,
+                       callback_user_chose_car.router,
                        carfax_get_handlers.router, car_to_find_handlers.router, admin_group_router,
                        unhandled_message.all_router)
-    #unhandled_message.all_router is alwaise last !!!!
-    #await asyncio.create_task(run_function_every_two_minutes(bot))
-   # await dp.start_polling(bot, skip_updates=True)
+    # unhandled_message.all_router is alwaise last !!!!
+    # await asyncio.create_task(run_function_every_two_minutes(bot))
+    # await dp.start_polling(bot, skip_updates=True)
     task1 = asyncio.create_task(run_function_every_two_minutes(bot))
     task2 = asyncio.create_task(dp.start_polling(bot, skip_updates=True))
     await asyncio.gather(task1, task2)
